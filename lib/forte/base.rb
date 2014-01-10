70fc71dbf1d46a9370a7f1c2b9a3a61237fe3829
Michael Yockey <mike@heysparkbox.com>
2014-01-10 12:08:07 -0500

    refactor: Replacing system() call for Git commands with ruby-git
    ruby-git still only wraps system() calls but supports all Git commands
    and has better error reporting

--- lib/forte/base.rb
+++ lib/forte/base.rb
@@ -1,8 +1,9 @@
 class Base < Thor
   desc "print", "Prints the public keys to STDOUT"
   def print(uri)
-    dir = Git.clone(uri)
-    puts AuthorizedKeys.build(dir)
-    Git.cleanup
+    loc = RepoLocation.new(uri)
+    repo = Git.clone(loc.uri, File.join(Dir.pwd, './.tmp'))
+    puts AuthorizedKeys.build(repo.dir.path)
+    FileUtils.rm_rf(repo.dir.path)
   end
 end
